c7a1f061d91f3388822a0dd47f0c7b9fc5af7348
Salim Khozami <khozami.salim@gmail.com>
2021-06-30 02:11:12 +0200

    Useful commands

--- shell linux.sh
+++ shell linux.sh
@@ -1,3 +1,15 @@
 # '' preserves all literals in shell but "" does not and also interpretes variable!
 
 echo '"$HOME"'
+
+# FOSS and FLOSS are two different philosopies of free and open/Libra software 
+# Licenses are not all the same, the most strict is GPL since it does not allow sublicencing !
+
+# cmds for sys admin works ... 
+w # list logged users
+
+last # last connected user
+
+source # or . Execute a file for example or read conf e.g [@]$. .profile 
+
+ 
